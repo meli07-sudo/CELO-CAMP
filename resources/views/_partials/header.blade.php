ec97a48a8297f61c3e4c5906d6a209d02fc7b041
Shazam72 <jfrancoiszouba72@gmail.com>
2021-10-29 13:50:59 +0200

    Créations comosants généraux

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,25 @@
+<div class="top-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-md col-xl-5 d-flex align-items-center">
+                <a class="navbar-brand align-items-center" href="index.html">
+                    <img src="./images/logo.png" alt="Logo K4A Blockchain" width="60">
+                </a>
+            </div>
+            <div class="col-md d-flex justify-content-end align-items-center">
+                <div class="social-media">
+                    <p class="mb-0 d-flex">
+                        <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+                        <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+                        <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+                        <a href="#" class="d-flex align-items-center justify-content-center"><span
+                                class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/_partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg  ftco-navbar-light" style="margin-top: 2em;">
+    <div class="container-xl">
+        <!-- <a class="navbar-brand align-items-center" href="index.html">
+    Academia
+    </a> -->
+        <a href="#" class="btn-custom order-lg-last">Obtenez votre certificat</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" href="index.html">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="courses.html">Cours</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">À propos</a></li>
+                <li class="nav-item"><a class="nav-link" href="team.html">L'équipe</a></li>
+                <li class="nav-item"><a class="nav-link" href="pricing.html">Prix</a></li>
+                <li class="nav-item"><a class="nav-link" href="blog.html">Forum</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contactez-nous</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
